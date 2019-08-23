c8a7aecd603647c9c983669fa4ad8fedf2969ebf
enricjorda <enric.jorda@codurance.com>
2019-08-23 15:23:44 +0200

    delete javascript version

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -5,23 +5,37 @@ import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TripServiceShould {
 
+    private User user;
     @Test
     void throw_exception_if_user_not_logged() {
+        FakeTripService fakeTripService = new FakeTripService();
 
-        User user = new User();
-        TripService tripService = new FakeNotLoggedTripService();
-
-
-        Assertions.assertThrows(UserNotLoggedInException.class,()->{tripService.getTripsByUser(user);});
+        assertThrows(UserNotLoggedInException.class,()->{fakeTripService.getTripsByUser(user);});
     }
 
-    class FakeNotLoggedTripService extends TripService{
+    @Test
+    void return_and_empty_list_for_non_friend_logged_user() {
+        FakeTripService fakeTripService = new FakeTripService();
+        fakeTripService.setFakeUser(new User());
+
+
+    }
+
+    class FakeTripService extends TripService {
+
+        private User fakeUser;
+
+        public void setFakeUser(User fakeUser) {
+            this.fakeUser = fakeUser;
+        }
 
         @Override
         protected User getLoggedUser() {
-            return null;
+            return this.fakeUser;
         }
     }
 }
